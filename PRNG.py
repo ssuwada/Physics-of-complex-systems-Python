7bff707be372aa01c7c8f9eae04ba64f65d6f617
Sebastian <127670534+ssuwada@users.noreply.github.com>
2023-03-17 19:31:34 +0100

    Update PRNG.py

--- PRNG.py
+++ PRNG.py
@@ -1,4 +1,4 @@
-#---------  FIRST CLASS EXERCISES -----------
+#---------  FIRST CLASS EXERCISES  -----------
 #   PRNG - PSEUDO RANDOM NUMBER GENERATOR
 #   IMPLEMENTATION, FUNCIONALITY AND PLOTS 
 #   WITH SAVING TO FILE GENERATED NUMBERS
@@ -8,7 +8,7 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt
-
+import random
 
 #--------- FUNC -------------
 #Functions - PRNG, ListplusOne
@@ -29,6 +29,15 @@ def list_plus_one(list1):
     list1.pop()
     return list_temp, list1
 
+def prgn_rand_func(a,c,N):
+    list1 = []
+    seed = random.uniform(0,1)
+    list1.append(seed)
+    for i in range(N):
+        xn = (a*list1[i]+c)%m
+        list1.append(xn)
+    return list1, seed    
+    
 
 #---------------- MAIN -----------------
 #   Main implementation of functions
@@ -40,7 +49,7 @@ m = pow(10,8) + 1
 seed = 0.023
 
 #   PRNG
-N = int(input('Number of elements: '))
+N = int(input('Number of elements ex 1: '))
 list1 = prgn(a,c,N,seed)
 
 #   Ex. 1 (b-i)
@@ -82,8 +91,8 @@ m = pow(2,31) + 1
 
 
 #   PRNG
-N2 = int(input('Number of elements: '))
-list1 = prgn(a,c,N,seed)
+N2 = int(input('Number of elements ex 2: '))
+list1 = prgn(a,c,N2,seed)
 
 #   Ex. 2 (b-i)
 #PLOT
@@ -109,4 +118,38 @@ plt.show()
 #   FILE OPEN AND WRITE
 f = open("LCG2-N"+str(N2)+"U"+str(seed)+".txt", "w")
 f.writelines(str(list1))
+f.close()
+
+
+#   Ex. 3
+
+
+N3 = int(input('Number of elements ex 3: '))
+
+temp3 = prgn_rand_func(a,c,N3)
+plt.plot(temp3[0])
+plt.show()
+
+
+#   Ex. 3 (b-ii)
+
+temp4 = list_plus_one(temp3[0])
+plt.scatter(temp4[0], temp4[1])
+plt.show()
+
+
+#   Ex. 3 (b-iii)
+
+
+temp5 = list_plus_one(temp4[0])
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(temp5[0],temp4[0],temp5[1])
+plt.show() 
+
+
+#   File write
+
+f = open("LCG1-N"+str(N)+"U"+str(temp3[1])+".txt", "w")
+f.writelines(str(temp3[0]))
 f.close()
